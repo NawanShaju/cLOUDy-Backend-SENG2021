25a49b06068ac46e373887631344b39beec43315
nishanshaju22 <nishan.shaju@icloud.com>
2026-03-07 15:24:21 +1100

    routes for order creation completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from .routes import main_bp
+from .routes import api
 
 def create_app(config_object="config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_object)
 
-    app.register_blueprint(main_bp, url_prefix="/api")
+    app.register_blueprint(api, url_prefix="/api")
 
     return app
