a6984ae6d3c7816aa80b20aa29f6fac4e5697b84
nishanshaju22 <nishan.shaju@icloud.com>
2026-03-16 19:30:16 +1100

    fixes for deployment

--- app/utils/extensions.py
+++ app/utils/extensions.py
@@ -1,9 +1,12 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask import request
 
 def get_api_key_identifier():
-    return request.headers.get("api-key") or get_remote_address()
+    from flask import request
+    try:
+        return request.headers.get("api-key") or get_remote_address()
+    except RuntimeError:
+        return get_remote_address()
 
 limiter = Limiter(
     key_func=get_api_key_identifier,
