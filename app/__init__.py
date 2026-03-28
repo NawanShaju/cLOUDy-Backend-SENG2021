601923fe0a2302309b98e629f66e40d748e30537
nishanshaju22 <nishan.shaju@icloud.com>
2026-03-28 13:18:46 +1100

    cors enabled for cross origin requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .utils.extensions import limiter
 from .routes import api, swaggerui_blueprint, register_swagger_yaml
+from flask_cors import CORS
 
 def create_app(config_object="config.Config"):
     app = Flask(__name__)
     app.config.from_object(config_object)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     
     limiter.init_app(app)
     
